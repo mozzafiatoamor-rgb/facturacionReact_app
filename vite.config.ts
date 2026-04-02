2970f662782d67ac31d917140bda722210609761
mozzafiatoamor-rgb <mozzafiatoamor@gmail.com>
2026-04-02 15:39:42 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, '.', '')
   const BASE = env.VITE_BASE_URL ?? '/'
 
   return {
